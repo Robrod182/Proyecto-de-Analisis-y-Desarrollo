735b5abb54a6c47d852f19be2e6272bb89b0b800
Robrod <rrodas182@gmail.com>
2020-11-13 17:12:06 -0600

    API REST
    API REST que exponen los proveedores de vehículos para que la empresa "Venta de vehículos S. A." pueda subastarlos. La mensajería se maneja en formato Json

---
+++ apivehiculos/conexion.php
@@ -0,0 +1,16 @@
+<?php
+
+$link = 'mysql:host=localhost;dbname=provehiculos';
+$usuario = 'root';
+$pass = '';
+
+try{
+
+    $pdo = new PDO($link,$usuario,$pass);
+
+   // echo 'Conectado';
+
+}catch (PDOException $e) {
+    print "¡Error!: " . $e->getMessage() . "<br/>";
+    die();
+} 
